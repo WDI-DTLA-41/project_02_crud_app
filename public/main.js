66945e3290198eaa6f47fc3ec9efece9f9033f3f
Barrett Quan <21barrett@gmail.com>
2017-01-17 10:36:37 -0800

    updated main.js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
 console.log('hi from main.js')
+
+$('createPost').on('click', function(evt) {
+  var message = $('#message').val();
+  $.post('/posts', {message: message}, function(res) {
+    $('ul').append('<li>' + message + '</li>');
+  });
+});
+
+$.get('/posts', function(res) {
+  var template = $('#posts-template').html();
+  var compiled = _.template(template);
+  var html = compiled({posts: res.posts});
+  $('#posts').html(html);
+});

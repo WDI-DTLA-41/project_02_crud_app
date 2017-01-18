b35673b25a0bfbb8196c36cf7842d266039416d1
Justin Samuels <blizm87@gmail.com>
2017-01-17 19:20:31 -0800

    setup member info form

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,33 @@ router.get('/', function(req, resp, next) {
 
 router.get('/login', function(req, resp, next){
   resp.render('login', title);
-})
+});
 
 router.get('/signup', function(req, resp, next){
   resp.render('signup', title);
-})
+});
+
+router.post('/createProfile', function(req, resp, next){
+
+  var memberInfo = {
+    firstName: req.body.firstName
+    // 'M.I.': req.body.midInit,
+    // lastName: req.body.lastName,
+    // userName: req.body.userName,
+    // password: req.body.password,
+    // email: req.body.emailAddress
+  }
+
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    db.collection('member').insertOne(memberInfo, function(err, result){
+      assert.equal(null, err);
+      console.log('Item inserted ' + result);
+      db.close();
+    });
+  });
+  resp.render('login', title);
+});
 
 // CREATE Data
 
